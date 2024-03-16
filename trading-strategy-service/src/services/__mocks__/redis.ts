bcc86e75d5ddad9dffbace5cf3591dcddedbb536
Michael Luo <michaelluo@Michaels-MacBook-Pro.local>
2024-03-16 20:35:24 +1100

    remove trading service

--- trading-strategy-service/src/services/__mocks__/redis.ts
+++
@@ -1,6 +0,0 @@
-const RedisMock = jest.fn().mockImplementation(() => ({
-  on: jest.fn(),
-  // Add other methods and properties as needed
-}));
-
-export default RedisMock;
--- trading-strategy-service/src/services/__mocks__/ws.ts
+++
@@ -1,7 +0,0 @@
-// __mocks__/ws.ts
-const WebSocketMock = jest.fn().mockImplementation(() => ({
-  on: jest.fn(),
-  // Add other methods and properties as needed
-}));
-
-export default WebSocketMock;
