292c104abca00b59802e919454720ab233357c14
Michael Luo <michaelluo@Michaels-MacBook-Pro.local>
2024-03-16 22:19:42 +1100

    update client service

--- client-service/src/index.ts
+++ client-service/src/index.ts
@@ -1,8 +1,11 @@
-// src/index.ts
+// client-service/src/index.ts
+import http from "http";
 import app from "./app";
+import { setupWebSocketServer } from "./services/socket.server";
 
-const PORT = process.env.PORT || 5002;
+const httpServer = http.createServer(app);
+const io = setupWebSocketServer(httpServer);
 
-app.listen(PORT, () => {
-  console.log(`Client Service listening on port ${PORT}`);
+httpServer.listen(5002, () => {
+  console.log("Client Service listening on port 5002");
 });
